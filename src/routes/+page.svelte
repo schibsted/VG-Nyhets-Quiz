95bbbebcf644641382e35802bf03ba3caaa4d003
Edvard Høiby <edvardhoiby@outlook.com>
2023-03-27 13:03:29 +0200

    feat: try a easy logic for question answering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,44 @@
 <script>
-    const appName = 'VG NyhetsQuiz'
+	const appName = 'VG NyhetsQuiz';
+	import { quizQuestions } from '../utils/quizQuestions';
+	import { onMount } from 'svelte';
+
+	let currentQuestionIndex = 0;
+	let currentScore = 0;
+
+	function handleAnswerSelect(answer) {
+		if (answer.isCorrect) {
+			currentScore++;
+		}
+
+		if (currentQuestionIndex < quizQuestions.length - 1) {
+			currentQuestionIndex++;
+		} else {
+			alert(`You scored ${currentScore} out of ${quizQuestions.length}!`);
+		}
+	}
+
+	onMount(() => {
+		quizQuestions.forEach((question) => {
+			question.answers.sort(() => Math.random() - 0.5);
+		});
+	});
 </script>
 
 <svelte:head>
-    <title>
-        {appName}
-    </title>
+	<title>
+		{appName}
+	</title>
 </svelte:head>
 
 <h1>{appName}</h1>
+<div>
+	{#if currentQuestionIndex < quizQuestions.length}
+		<p>{quizQuestions[currentQuestionIndex].question}</p>
+		{#each quizQuestions[currentQuestionIndex].answers as answer}
+			<button on:click={() => handleAnswerSelect(answer)}>
+				{answer.text}
+			</button>
+		{/each}
+	{/if}
+</div>
