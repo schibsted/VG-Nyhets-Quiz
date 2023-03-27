e9cf30abd97f260c5d06e08282dccaf364102fe8
Edvard Høiby <edvardhoiby@outlook.com>
2023-03-27 13:02:58 +0200

    feat: add mock quiz questions

---
+++ src/utils/quizQuestions.ts
@@ -0,0 +1,129 @@
+export const quizQuestions = [
+	{
+		question: 'Hvilken norsk roer med OL-gull er dette?',
+		answers: [
+			{ text: 'Nils Jakob Hoff', isCorrect: false },
+			{ text: 'Olaf Tufte', isCorrect: true },
+			{ text: 'Kristoffer Brun', isCorrect: false },
+			{ text: 'Are Strandli', isCorrect: false }
+		]
+	},
+	{
+		question: 'I hvilken verdensdel ligger Kamerun og Angola?',
+		answers: [
+			{ text: 'Europa', isCorrect: false },
+			{ text: 'Afrika', isCorrect: true },
+			{ text: 'Asia', isCorrect: false },
+			{ text: 'Sør-Amerika', isCorrect: false }
+		]
+	},
+	{
+		question: 'I hvilket år ble Gro Harlem Brundtland vår første kvinnelige statsminister?',
+		answers: [
+			{ text: '1981', isCorrect: true },
+			{ text: '1990', isCorrect: false },
+			{ text: '1975', isCorrect: false },
+			{ text: '2005', isCorrect: false }
+		]
+	},
+	{
+		question: 'Fullfør navnet på dette firmaet kjent for å lage våpen: Smith &...',
+		answers: [
+			{ text: 'Johnson', isCorrect: false },
+			{ text: 'Wesson', isCorrect: true },
+			{ text: 'Weston', isCorrect: false },
+			{ text: 'Watson', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hvilken organisasjon forkortes på fransk OTAN?',
+		answers: [
+			{ text: 'FN', isCorrect: false },
+			{ text: 'EU', isCorrect: false },
+			{ text: 'NATO', isCorrect: true },
+			{ text: 'OECD', isCorrect: false }
+		]
+	},
+	{
+		question:
+			'Hvem har laget «Cats», «The Phantom of the Opera», «Jesus Christ Superstar» og «Evita»?',
+		answers: [
+			{ text: 'Elton John', isCorrect: false },
+			{ text: 'Andrew Lloyd Webber', isCorrect: true },
+			{ text: 'Stephen Sondheim', isCorrect: false },
+			{ text: 'Tim Rice', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hvilken farge er det mest av i det samiske flagget?',
+		answers: [
+			{ text: 'Blå', isCorrect: true },
+			{ text: 'Grønn', isCorrect: false },
+			{ text: 'Rød', isCorrect: false },
+			{ text: 'Gul', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hvilket ord på A brukes også om slitasjegikt i leddene?',
+		answers: [
+			{ text: 'Astma', isCorrect: false },
+			{ text: 'Alzheimers', isCorrect: false },
+			{ text: 'Artrose', isCorrect: true },
+			{ text: 'AIDS', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hva heter forfatteren av «American Psycho» som nå er aktuell med boken «Skår»?',
+		answers: [
+			{ text: 'Bret Easton Ellis', isCorrect: true },
+			{ text: 'Stephen King', isCorrect: false },
+			{ text: 'Gillian Flynn', isCorrect: false },
+			{ text: 'Donna Tartt', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hva heter duoen som man kan støte på om man bor på et Ving-hotell?',
+		answers: [
+			{ text: 'Lollo og Bernie', isCorrect: true },
+			{ text: 'Bert og Ernie', isCorrect: false },
+			{ text: 'Lena og Linda', isCorrect: false },
+			{ text: 'Pelle og Kalle', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hvilken kanal skiller halvøya Peloponnes fra resten av Hellas?',
+		answers: [
+			{ text: 'Suezkanalen', isCorrect: false },
+			{ text: 'Dardanellene', isCorrect: false },
+			{ text: 'Korintkanalen', isCorrect: true },
+			{ text: 'Panamakanalen', isCorrect: false }
+		]
+	},
+	{
+		question: 'I hvilken by skal sommer-OL avholdes i 2032?',
+		answers: [
+			{ text: 'Paris', isCorrect: false },
+			{ text: 'Brisbane', isCorrect: true },
+			{ text: 'Los Angeles', isCorrect: false },
+			{ text: 'Beijing', isCorrect: false }
+		]
+	},
+	{
+		question: 'I hvilket land ligger byene Budvar, Kotor og Cetinje?',
+		answers: [
+			{ text: 'Kroatia', isCorrect: false },
+			{ text: 'Serbia', isCorrect: false },
+			{ text: 'Montenegro', isCorrect: true },
+			{ text: 'Bosnia-Hercegovina', isCorrect: false }
+		]
+	},
+	{
+		question: 'Hvilken lege og komiker er aktuell med showet «Uten grenser»?',
+		answers: [
+			{ text: 'Johan Golden', isCorrect: false },
+			{ text: 'Nina Lykkes', isCorrect: false },
+			{ text: 'Jonas Kinge Bergland', isCorrect: true },
+			{ text: 'Anne-Kat Hærland', isCorrect: false }
+		]
+	}
+];
