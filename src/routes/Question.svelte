509d847d3440a43f5937ec25684ac193a78bd429
Edvard Høiby <edvardhoiby@outlook.com>
2023-03-28 13:37:35 +0200

    feat: checkmark icon

--- src/routes/Question.svelte
+++ src/routes/Question.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { QuizQuestion, UserAnswer } from '../types/QuizQuestions';
+	import CheckmarkIcon from './Icons/CheckmarkIcon.svelte';
 
 	export let question: QuizQuestion;
 	export let handleAnswerSelect: (answerIndex: number) => void;
@@ -14,17 +15,20 @@
 	{#each question.answers as answer, index}
 		<button
 			class="
-            p-4 rounded-xl border {userAnswer === index
+            p-4 flex row gap-4 rounded-xl border {userAnswer === index
 				? 'border-[#1b1b1d]'
 				: 'border-[#3C3C43]/29'} {userAnswer !== null && userAnswer !== undefined
 				? answer.isCorrect
-					? 'bg-green-100'
-					: 'bg-red-100'
+					? 'bg-green-50'
+					: 'bg-red-50'
 				: 'bg-white'}"
 			on:click={() => handleAnswerSelect(index)}
 			disabled={isAnswered}
 		>
-			{userAnswer === index ? 'SVAR' : ''}{answer.text}
+			{#if userAnswer === index}
+				<CheckmarkIcon />
+			{/if}
+			{answer.text}
 		</button>
 	{/each}
 </div>
